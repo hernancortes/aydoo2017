00a4408372d561776f6abcc44ae5e0058d474433
Hernan Cortes <hernancorteshc@hotmail.com>
2017-04-17 15:18:00 -0300

    Primer commit de factores primos 3, base de factores primos 2

---
+++ factores-primos-3/src/main/java/ar/edu/untref/aydoo/Program.java
@@ -0,0 +1,90 @@
+package ar.edu.untref.aydoo;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.ListIterator;
+
+public class Program{
+    
+    public static final int DOSARGUMENTOS = 2;  // defino MAGIC NUMBER
+
+    public static final void main(String[] arg) throws Exception{
+        Program programa = new Program();
+    	try{
+            programa.imprimirResultadoConFormato(Integer.parseInt(arg[0]), programa.buscarPrimos(Integer.parseInt(arg[0])), validarTipoDeFormato(arg));
+        } catch (Exception e) {
+            impresionDeMensaje("El numero ingresado no es valido");
+        }
+    }
+    
+    public static String validarTipoDeFormato(String[] arg){
+        String formatoParaImprimir;
+        //este if verifica que si el usuario no ingreso tipo de formato, el formato no se pase como argumento nulo
+        if (arg.length < DOSARGUMENTOS) formatoParaImprimir = "--format=pretty";
+        else formatoParaImprimir = arg[1].toLowerCase();
+        return formatoParaImprimir;
+    }
+            
+    public static void impresionDeMensaje(String textoAImprimir){
+        System.out.print(textoAImprimir);
+    }
+    
+    public ArrayList<Integer> buscarPrimos(int numeroIngresado){
+    	int divisorCandidato = 2;
+        ArrayList<Integer> listaDeDivisoresPrimos = new ArrayList<>();
+    	while (!esPrimo(numeroIngresado)){
+            if (numeroIngresado % divisorCandidato == 0){
+    		numeroIngresado = numeroIngresado/divisorCandidato;
+                listaDeDivisoresPrimos.add(divisorCandidato);
+            } else {
+    		divisorCandidato++;
+    		while (!esPrimo(divisorCandidato)) divisorCandidato++;
+            }
+    	}
+        listaDeDivisoresPrimos.add(numeroIngresado);
+        return listaDeDivisoresPrimos;
+    }
+    
+    public boolean esPrimo(int numeroIngresado){
+    	if (numeroIngresado == 1 || numeroIngresado == 2 || numeroIngresado == 3){
+            return true;
+    	} else {
+            if (numeroIngresado % 2 == 0){
+                return false;
+            } else {
+                double maximoAprox = Math.round(Math.sqrt(numeroIngresado));
+                int maximo = (int) maximoAprox;
+                for (int i=3; i<=maximo; i++){
+                    if (numeroIngresado % i == 0) return false;
+                    i++;
+                }
+                return true;
+            }
+        }
+    }
+    
+    public void imprimirResultadoConFormato(int numeroIngresado, ArrayList<Integer> listaDeDivisoresPrimos, String formatoDeImpresionIngresado){
+        switch (formatoDeImpresionIngresado){
+            case "--format=pretty":
+                imprimirConFormatoPretty(numeroIngresado, listaDeDivisoresPrimos);
+                break;
+            case "--format=quiet":
+                imprimirConFormatoQuiet(listaDeDivisoresPrimos);
+                break;
+            default:
+                impresionDeMensaje("Formato no aceptado. Las opciones posibles son: pretty o quiet.");
+        }
+    }
+    
+    public void imprimirConFormatoPretty(int numeroIngresado, ArrayList<Integer> listaDeDivisoresPrimos){
+        impresionDeMensaje("Factores primos " + numeroIngresado + ": ");
+    	Iterator<Integer> iterador = listaDeDivisoresPrimos.iterator();
+    	while(iterador.hasNext()) impresionDeMensaje(String.valueOf(iterador.next()) + " ");
+    }
+    
+    public void imprimirConFormatoQuiet(ArrayList<Integer> listaDeDivisoresPrimos){
+    	ListIterator iterador = listaDeDivisoresPrimos.listIterator(listaDeDivisoresPrimos.size());
+        while(iterador.hasPrevious()) impresionDeMensaje(String.valueOf(iterador.previous())+ "\n");
+    }
+    
+}
---
+++ factores-primos-3/src/test/java/ar/edu/untref/aydoo/ProgramTest.java
@@ -0,0 +1,101 @@
+package ar.edu.untref.aydoo;
+
+import java.util.ArrayList;
+import org.junit.Test;
+import org.junit.Assert;
+
+
+public class ProgramTest 
+{
+    @Test
+    public void ingresoNumeroOchoyPrueboQueNoEsPrimo(){
+        Program nuevoProg = new Program();
+        boolean resultado = nuevoProg.esPrimo(8);
+        boolean resultadoEsperado = false;
+        Assert.assertEquals(resultadoEsperado, resultado);
+    }
+    
+    @Test
+    public void ingresoNumeroDosYPrueboQueSiEsPrimo(){
+        Program nuevoProg = new Program();
+        boolean resultado = nuevoProg.esPrimo(2);
+        boolean resultadoEsperado = true;
+        Assert.assertEquals(resultadoEsperado, resultado);
+    }
+    
+    @Test
+    public void ingresoListaTestConNumeroDosYComparoConBuscarPrimosDeNumeroDos(){
+        Program nuevoProg = new Program();
+        ArrayList<Integer> listaTest = new ArrayList<>();
+        listaTest.add(2);
+        Assert.assertEquals(listaTest,nuevoProg.buscarPrimos(2));
+    }
+    
+    @Test
+    public void ingresoListaTestConNumeroDosYTresYComparoConBuscarPrimosDeNumeroDos(){
+        Program nuevoProg = new Program();
+        ArrayList<Integer> listaTest = new ArrayList<>();
+        listaTest.add(2);
+        listaTest.add(2);
+        Assert.assertNotEquals(listaTest,nuevoProg.buscarPrimos(2));
+    }
+    
+    @Test
+    public void ingresoListaTestConNumerosPrimosDe360YComparoConBuscarPrimosDe360(){
+        Program nuevoProg = new Program();
+        ArrayList<Integer> listaTest = new ArrayList<>();
+        listaTest.add(2);
+        listaTest.add(2);
+        listaTest.add(2);
+        listaTest.add(3);
+        listaTest.add(3);
+        listaTest.add(5);
+        Assert.assertEquals(listaTest,nuevoProg.buscarPrimos(360));
+    }
+    
+    @Test
+    public void ingresoListaTestConNumerosPrimosDe360YUnoDeEllosErroneoYComparoConBuscarPrimosDe360(){
+        Program nuevoProg = new Program();
+        ArrayList<Integer> listaTest = new ArrayList<>();
+        listaTest.add(2);
+        listaTest.add(2);
+        listaTest.add(2);
+        listaTest.add(4);
+        listaTest.add(3);
+        listaTest.add(5);
+        Assert.assertNotEquals(listaTest,nuevoProg.buscarPrimos(360));
+    }
+    
+    @Test
+    public void ingresoFormatoQuietEnMayusculasYLuegoDeValidarSeraConvertidoAMinusculas(){
+        Program nuevoProg = new Program();
+        String[] listaArgumentosTest = new String[2];
+        listaArgumentosTest[0] = "360";
+        listaArgumentosTest[1] = "--format=QUIET";
+        String resultadoEsperado = "--format=quiet";
+        String resultado = nuevoProg.validarTipoDeFormato(listaArgumentosTest);
+        Assert.assertEquals(resultadoEsperado, resultado);
+    }
+    
+    @Test
+    public void ingresoFormatoPrettyEnMayusculasYMinusculasYLuegoDeValidarSeraConvertidoAMinusculas(){
+        Program nuevoProg = new Program();
+        String[] listaArgumentosTest = new String[2];
+        listaArgumentosTest[0] = "360";
+        listaArgumentosTest[1] = "--format=PreTTy";
+        String resultadoEsperado = "--format=pretty";
+        String resultado = nuevoProg.validarTipoDeFormato(listaArgumentosTest);
+        Assert.assertEquals(resultadoEsperado, resultado);
+    }
+        
+    @Test
+    public void noIngresoFormatoEntoncesLuegoDeValidarDevolveraFormatoPretty(){
+        Program nuevoProg = new Program();
+        String[] listaArgumentosTest = new String[1];
+        listaArgumentosTest[0] = "360";
+        String resultadoEsperado = "--format=pretty";
+        String resultado = nuevoProg.validarTipoDeFormato(listaArgumentosTest);
+        Assert.assertEquals(resultadoEsperado, resultado);
+    }
+    
+}
